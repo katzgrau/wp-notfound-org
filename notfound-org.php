b9c31a4e2b58810613e0d896437aee269de0f897
Arno Van Biesen <arno.vanbiesen@famousgrey.com>
2021-09-03 16:37:45 +0200

    remove name

--- notfound-org.php
+++ notfound-org.php
@@ -4,7 +4,7 @@ Plugin Name: NotFound.org 404 Page
 Plugin URI: https://github.com/katzgrau/wp-notfound-org
 Description: A Wordpress plugin that turns your 404 "not found" error into a page devoted to helping find missing children.
 Version: 1.2.0
-Author: Kenny Katzgrau & Arno Van Biesen
+Author: Kenny Katzgrau
 Author URI: http://codefury.net
 */
 
